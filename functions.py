1514ea4e69eb158f5075255adf84e32ad9ddacfa
theweekday <104345724+ryanwtl@users.noreply.github.com>
2025-03-08 20:42:02 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import torch
 import emoji
 
 models = {}
+tokenizers = {}
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,21 +14,20 @@ def load_model_tokenizer(token):
     model_name = ""
     model_path = "theweekday/personality_traits_"
 
-    tokenizer = RobertaTokenizer.from_pretrained(model_path, token=token)
-
     for trait in ['openness', 'conscientiousness', 'extraversion', 'agreeableness', 'neuroticism']:
         path = f"{model_path}{trait}"
         model = RobertaForSequenceClassification.from_pretrained(path, token=token, num_labels=1)
-        model.to(device)
+        tokenizer = RobertaTokenizer.from_pretrained(model_path, token=token)
+        tokenizers[trait] = tokenizer
         models[trait] = model 
-    return models, tokenizer
+    return models, tokenizers
 
 def personality_analysis_sentence(sentence, models, tokenizer, max_length=512):
     analysis = {}
     components = ['value', 'score']
     for trait, model in models.items():
         model.to(device)
-        encodings = tokenizer([sentence], truncation=True, padding=True, max_length=max_length, return_tensors='pt').to(device)
+        encodings = tokenizers[trait]([sentence], truncation=True, padding=True, max_length=max_length, return_tensors='pt').to(device)
         model.eval()
         with torch.no_grad():
             output = {}
