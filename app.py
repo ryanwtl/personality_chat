3cf24d4b6e448e37cac364125d0bfe96a6f41306
theweekday <104345724+ryanwtl@users.noreply.github.com>
2025-03-11 13:48:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 from groq import Groq
 import functions as f
-import requests
-import json
 import time
-import os
 
 user_id = "user1"
-
-AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
 # Initialize Groq client
 client = Groq()
@@ -42,6 +37,19 @@ user_input = st.chat_input("Type your message...")
 if user_input:
     # Add user message to session state
     st.session_state.messages.append({"role": "user", "content": user_input})
+
+    if user_input.strip().lower() == "/bye":
+        user_messages = [{'content': message['content']} for message in st.session_state.messages if message['role'] == 'user']
+
+        analysis = f.end_of_chat(user_messages, st.session_state.models, st.session_state.tokenizers)
+        st.session_state.messages = []
+        with st.chat_message("assistant"):
+            st.markdown(f"Thank you for chatting with me. Goodbye!")
+            st.markdown(f"Personality Analysis:")
+            for trait, details in analysis.items():
+                st.markdown(f"**{trait.capitalize()}**: {details['value'].upper()} (Score: {details['score']})")
+        st.stop()
+
     with st.chat_message("user"):
         st.markdown(user_input)
 
@@ -77,8 +85,10 @@ if st.button("Clear Chat"):
 st.sidebar.header("Personality Traits")
 if st.session_state.messages:
     latest_user_message = next((msg['content'] for msg in reversed(st.session_state.messages) if msg['role'] == 'user'), "")
+    messages_analyze = "\n".join([msg["content"] for msg in st.session_state.messages if msg["role"] == "user"])
+    print(f"\nmessages_analyze : {messages_analyze}\n")
     if latest_user_message:
-        converted = f.convert_emojis(latest_user_message)
+        converted = f.convert_emojis(messages_analyze)
 
         start_time = time.time()
         traits = f.personality_analysis_sentence(converted, st.session_state.models, st.session_state.tokenizers)
@@ -86,7 +96,6 @@ if st.session_state.messages:
 
         print(f"\npersonality_analysis_sentence() : {analysis_time}\n")
 
-        headers = {"Authorization": f"Bearer {AUTH_TOKEN}"}
         record = {"sender_id": user_id,"text": converted, "traits": traits}
 
         print(f"\nrecord : {record}\n")
