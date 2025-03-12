48999c20d45c1a8489b2e501703a6e39a0e368bd
theweekday <104345724+ryanwtl@users.noreply.github.com>
2025-03-12 20:40:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ client = Groq()
 
 start_time = time.time()
 # Load model and tokenizer once
+
 if "models" not in st.session_state or "tokenizers" not in st.session_state:
     st.session_state.models, st.session_state.tokenizers = f.load_model_tokenizer()
 
 print(f"\nload_model_tokenizer() : {time.time() - start_time}\n")
 
 # Streamlit app setup
-st.set_page_config(page_title="LLaMA 3.3 Chat Room", layout="wide")
+st.set_page_config(page_title="Property Preference Analysis", layout="wide")
 
 # App header
 st.title("💬 Chatroom for Prospect")
@@ -40,14 +41,29 @@ if user_input:
 
     if user_input.strip().lower() == "/bye":
         user_messages = [{'content': message['content']} for message in st.session_state.messages if message['role'] == 'user']
+        messages = "\n".join([msg["content"] for msg in user_messages[:-1]])
+        print(f"\nmessages : {messages}\n")
 
-        analysis = f.end_of_chat(user_messages, st.session_state.models, st.session_state.tokenizers)
+        traits = f.personality_analysis_sentence(messages, st.session_state.models, st.session_state.tokenizers)
+        converted = f.convert_emojis(messages)
+        validation_response = f.validate_personality_with_llm(converted, traits, client)
+        property_recommendation = f.property_recommend(user_id, traits, client)
+        df = f.analysis_result_output2(traits)
+        sdf = f.analysis_result_output2(st.session_state.straits)
+
         st.session_state.messages = []
         with st.chat_message("assistant"):
-            st.markdown(f"Thank you for chatting with me. Goodbye!")
-            st.markdown(f"Personality Analysis:")
-            for trait, details in analysis.items():
-                st.markdown(f"**{trait.capitalize()}**: {details['value'].upper()} (Score: {details['score']})")
+            st.markdown(f"Thank you for chatting with me. Goodbye! Here are your results:")
+            st.markdown("### Personality Traits Analysis - Previous Sentence:")
+            st.dataframe(sdf, use_container_width=True)
+
+            st.markdown("### Personality Traits Analysis - Complete Convo:")
+            st.dataframe(df, use_container_width=True)
+
+            st.markdown(f"### Property Recommendation:")
+            st.markdown(f"{property_recommendation}")
+            st.markdown(f"### Validation Response:")
+            st.markdown(f"{validation_response}")
         st.stop()
 
     with st.chat_message("user"):
@@ -85,34 +101,22 @@ if st.button("Clear Chat"):
 st.sidebar.header("Personality Traits")
 if st.session_state.messages:
     latest_user_message = next((msg['content'] for msg in reversed(st.session_state.messages) if msg['role'] == 'user'), "")
-    messages_analyze = "\n".join([msg["content"] for msg in st.session_state.messages if msg["role"] == "user"])
-    print(f"\nmessages_analyze : {messages_analyze}\n")
+
     if latest_user_message:
-        converted = f.convert_emojis(messages_analyze)
+        converted = f.convert_emojis(latest_user_message)
 
         start_time = time.time()
-        traits = f.personality_analysis_sentence(converted, st.session_state.models, st.session_state.tokenizers)
+        st.session_state.straits = f.personality_analysis_sentence(converted, st.session_state.models, st.session_state.tokenizers)
         analysis_time = time.time() - start_time
 
         print(f"\npersonality_analysis_sentence() : {analysis_time}\n")
 
-        record = {"sender_id": user_id,"text": converted, "traits": traits}
+        record = {"sender_id": user_id,"text": converted, "traits": st.session_state.straits}
 
         print(f"\nrecord : {record}\n")
-        print(f"\nrecord_type : {type(record)}\n")
                 
-        validation_response = f.validate_personality_with_llm(converted, traits, client)
-        property_recommendation = f.property_recommend(user_id, traits, client)
+        st.session_state.svalidation_response = f.validate_personality_with_llm(converted, st.session_state.straits, client)
+        st.session_state.sproperty_recommendation = f.property_recommend(user_id, st.session_state.straits, client)
         
-        for trait, details in traits.items():
+        for trait, details in st.session_state.straits.items():
             st.sidebar.write(f"**{trait.capitalize()}**: {details['value'].upper()} (Score: {details['score']})")
-
-        # Instructions for running the app
-        st.sidebar.markdown("---")
-        st.sidebar.header("Property Recommendation")
-        st.sidebar.write(f"Property Recommendation: \n{property_recommendation}")
-
-        st.sidebar.markdown("---")
-        st.sidebar.header("Validation with LLaMA")
-        st.sidebar.markdown("To validate the personality traits, make sure to run the receiver script in a separate terminal.")
-        st.sidebar.write(f"Validation Response: \n{validation_response}")
